2b6ea28d9c6c8d71d72cf64d0838b50f58c4b7b8
cumbinar <cumbiweb@gmail.com>
2022-07-26 15:11:47 -0500

    create proyect datos primitivos

---
+++ Primitivos/src/main/java/com/mycompany/primitivos/Primitivos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.primitivos;
+
+/**
+ *
+ * @author educu
+ */
+public class Primitivos {
+
+    public static void main(String[] args) {
+        
+        //Tipos primitivos enteros: byte, short, int, long
+        System.out.println("Tipos primitivos enteros: byte, short, int, long");
+        System.out.println("Valor mínimo byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor máximo byte: " + Byte.MAX_VALUE);
+        System.out.println("Valor mínimo int: " + Integer.MIN_VALUE);
+        System.out.println("Valor máximo int: " + Integer.MAX_VALUE);
+        
+    }
+}
