1d6452f0d2a4e7a795ff1cedb142bffffd3fa4cf
cumbinar <cumbiweb@gmail.com>
2022-07-30 11:17:49 -0500

    create conexion to Bd

--- Swing/src/main/java/modelo/Conexion.java
+++ Swing/src/main/java/modelo/Conexion.java
@@ -4,6 +4,11 @@
  */
 package modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet; //La clase que va a recibir los datos de consultas
+import java.sql.SQLException;
+import java.sql.Statement; //para crear sentencias SQL
 
 /**
  *
@@ -11,6 +16,40 @@ package modelo;
  */
 public class Conexion {
 
-    
+    private Connection connection;
+
+    public Conexion() {
+        connect(); //abro el archivo .db con la base de datos
+    }
+
+    private void connect() {
+        /**
+         * Este método va a ir al archivo .db y cargarlo en un objeto de la
+         * clase Connection
+         */
+        connection = null;
+        try {
+            String path = "jdbc:sqlite:directorio.db"; //nombre de la db a abrir
+            connection = DriverManager.getConnection(path); //obtiene la conexion en el objeto connection
+            System.out.println("Conexión Lograda");
+        } catch (SQLException e) { //para evitar que el programa se detenga si hay algún error
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public ResultSet ejecutarQuery(String query) {
+        /**
+         * Este método recibe una query (consulta) en formato string y la
+         * ejecuta en la base de datos.
+         */
+        ResultSet result = null;
+        try {
+            Statement stmt = connection.createStatement();
+            result = stmt.executeQuery(query); //ejecuto la query en la db
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
 
 }
