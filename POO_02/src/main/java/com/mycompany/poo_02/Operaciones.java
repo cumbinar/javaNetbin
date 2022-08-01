743da5df0a0a128afc180fa348e0ec2ec2c73b87
cumbinar <cumbiweb@gmail.com>
2022-08-01 10:15:25 -0500

    create metods operations

--- POO_02/src/main/java/com/mycompany/poo_02/Operaciones.java
+++ POO_02/src/main/java/com/mycompany/poo_02/Operaciones.java
@@ -4,19 +4,50 @@
  */
 package com.mycompany.poo_02;
 
+import javax.swing.*;
+
 /**
  *
  * @author educu
  */
 public class Operaciones {
     //Atributos
-    
+
     float numero1;
     float numero2;
     float suma;
     float resta;
     float multiplicar;
     float dividir;
-    
+
     //Métodos
+    //Método para pedir al usuario dos números
+    public void leerNumeros() {
+        numero1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el primer número: "));
+        numero2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el primer número: "));
+    }
+
+    public void sumar() {
+        suma = numero1 + numero2;
+    }
+
+    public void restar() {
+        resta = numero1 - numero2;
+    }
+
+    public void multiplicacion() {
+        multiplicar = numero1 * numero2;
+    }
+
+    public void dividision() {
+        dividir = numero1 / numero2;
+    }
+
+    public void muestraRsultados() {
+        System.out.println("La Suma de : " + numero1 + " + " + numero2 + " Es " + suma);
+        System.out.println("La Resta de : " + numero1 + " - " + numero2 + " Es " + resta);
+        System.out.println("La División de : " + numero1 + " / " + numero2 + " Es " + dividir);
+        System.out.println("La Multiplicación de : " + numero1 + " * " + numero2 + " Es " + multiplicar);
+
+    }
 }
