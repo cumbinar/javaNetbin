bf3e0b6e4426fe9b9c1a5ab0d8cfca388d67bb68
cumbinar <cumbiweb@gmail.com>
2022-07-31 19:59:49 -0500

    actualize Funciones class

--- Funciones/src/main/java/com/mycompany/funciones/Funciones.java
+++ Funciones/src/main/java/com/mycompany/funciones/Funciones.java
@@ -16,8 +16,12 @@ public class Funciones {
         int a = 10;
         int b = 25;
         System.out.println("el resultado sumar:  " + a + " + " + b + " es " + suma(a, b));
+
+        mayorQue(a, b);
         
-        mayorQue(a,b);
+        char caracter = '/';
+        System.out.println("El caracter " + caracter + " tiene el código ASCII: " + codigoAscii(caracter));
+        
 
     }
 
@@ -28,21 +32,22 @@ public class Funciones {
     public static int suma(int a, int b) {
         return a + b;
     }
-    
-    public static void mayorQue(int a, int b){
-        int mayor =0;
-         if (a>b){
-         mayor = a;
-         } else{
-          mayor = b;
-         };
-         
-         System.out.println("a=" + a + " , b="+ b + " El número mayor es: "+ mayor);
+
+    public static void mayorQue(int a, int b) {
+        int mayor = 0;
+        if (a > b) {
+            mayor = a;
+        } else {
+            mayor = b;
+        }
+
+        System.out.println("a=" + a + " , b=" + b + " El número mayor es: " + mayor);
     }
     
+    public static int codigoAscii(char caracter){
     
+         return (int)caracter; //casting a entero un string pasado como parametro
+         
+    }
+
 }
-
-
-
-
