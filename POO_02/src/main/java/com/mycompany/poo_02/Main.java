f508374294fe5667a33870fc87f9b1c877f3b901
cumbinar <cumbiweb@gmail.com>
2022-08-01 10:27:13 -0500

    create Main class

---
+++ POO_02/src/main/java/com/mycompany/poo_02/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_02;
+
+/**
+ *
+ * @author educu
+ */
+public class Main {
+    public static void main(String [] args){
+    Operaciones calcular = new Operaciones(); //se instancia la clase Operaciones
+    calcular.leerNumeros();
+    }
+}
