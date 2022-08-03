f4f79ccb37138637f1e36adbe2b14c7018968250
cumbinar <cumbiweb@gmail.com>
2022-08-02 19:23:36 -0500

    define methods Operacion class

---
+++ POO_04/src/main/java/com/mycompany/poo_04/Operacion.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_04;
+
+/**
+ *
+ * @author educu
+ */
+public class Operacion {
+
+    public float sumar(float numero1, float numero2) {
+        float suma = numero1 + numero2;
+        return suma;
+    }
+
+    public float restar(float numero1, float numero2) {
+        float resta = numero1 - numero2;
+        return resta;
+    }
+
+    public float multiplicacion(float numero1, float numero2) {
+        float multiplicar = numero1 * numero2;
+        return multiplicar;
+    }
+
+    public float division(float numero1, float numero2) {
+        float dividir = numero1 / numero2;
+        return dividir;
+    }
+
+}
