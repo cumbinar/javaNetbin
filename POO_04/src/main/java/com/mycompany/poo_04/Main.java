05dd501e2b0140c3813a4430e80cdd2e75ae445b
cumbinar <cumbiweb@gmail.com>
2022-08-02 19:28:08 -0500

    actualize main class

--- POO_04/src/main/java/com/mycompany/poo_04/Main.java
+++ POO_04/src/main/java/com/mycompany/poo_04/Main.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.poo_04;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author educu
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el Primer número: "));
+        float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el Segundo número: "));
+        
+    }
 }
