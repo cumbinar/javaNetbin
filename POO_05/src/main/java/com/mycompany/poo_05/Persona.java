657544033707bdb32737a5685b9466d306ab418c
cumbinar <cumbiweb@gmail.com>
2022-08-03 16:04:39 -0500

    create Persona class with methods

---
+++ POO_05/src/main/java/com/mycompany/poo_05/Persona.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_05;
+
+/**
+ *
+ * @author educu
+ */
+public class Persona {
+    //Atributos
+    
+    String nombre;
+    int edad;
+    
+    //Métodos
+    
+    //Método constructor, ayuda a inicializar los atributos de la clase
+    
+    public Persona(String _nombre, int _edad){
+    
+    nombre = _nombre;
+    edad = _edad;
+    }
+    
+    public void mostrarDatos(){
+        System.out.println("El nombre es: "+ nombre);
+        System.out.println("La edade es: "+ edad);
+    }
+}
