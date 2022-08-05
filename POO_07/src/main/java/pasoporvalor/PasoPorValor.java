29367e5ce11acbf4329f3cbe7be4df625b70e4a4
cumbinar <cumbiweb@gmail.com>
2022-08-05 16:34:32 -0500

    create PasoPorValor class

--- POO_07/src/main/java/pasoporvalor/PasoPorValor.java
+++ POO_07/src/main/java/pasoporvalor/PasoPorValor.java
@@ -9,5 +9,15 @@ package pasoporvalor;
  * @author educu
  */
 public class PasoPorValor {
+    public static void main(String[] args) {
+      var x = 10;  
+        System.out.println(" X "+ x);
+        
+        cambioValor(x);
+    }
     
+    public static void cambioValor(int arg1){
+        System.out.println("arg1 = " + arg1);
+    }
+ 
 }
