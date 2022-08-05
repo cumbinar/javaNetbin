2791469549825709a61327ba9932fff179ec89cb
cumbinar <cumbiweb@gmail.com>
2022-08-04 22:46:47 -0500

    create PruebaAritmetica.java and Main.java class

---
+++ POO_06/src/main/java/com/mycompany/poo_06/PruebaAritmetica.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_06;
+
+/**
+ *
+ * @author educu
+ */
+public class PruebaAritmetica {
+
+    float numero1;
+    float numero2;
+
+    public void sumarConArgumentos(float arg1, float arg2) {
+
+        numero1 = arg1;
+        numero2 = arg2;
+        float suma = arg1 + arg2;
+        System.out.println("El resultado de sumar " + numero1 + " + " + numero2 + " es => " + suma);
+    }
+}
