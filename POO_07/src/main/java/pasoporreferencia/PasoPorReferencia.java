e24f9fca8a9294221bbf80c576043551ec723f94
cumbinar <cumbiweb@gmail.com>
2022-08-05 16:49:04 -0500

    create pasoporreferencia package

---
+++ POO_07/src/main/java/pasoporreferencia/PasoPorReferencia.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pasoporreferencia;
+import clases.Persona;
+/**
+ *
+ * @author educu
+ */
+public class PasoPorReferencia {
+    public static void main(String[] args) {
+        
+    }
+}
