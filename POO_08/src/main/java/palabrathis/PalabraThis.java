a7f0df7dc171158ab4df67028d9b5c5e0cb4f12a
cumbinar <cumbiweb@gmail.com>
2022-08-06 11:20:57 -0500

    create POO_08/ proyect

---
+++ POO_08/src/main/java/palabrathis/PalabraThis.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package palabrathis;
+
+/**
+ *
+ * @author educu
+ */
+public class PalabraThis {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
