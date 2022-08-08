2fc7bd82fef550840e97f680045ade4083606e74
cumbinar <cumbiweb@gmail.com>
2022-08-07 21:05:52 -0500

    create personaPrueba class

--- Encapsulamiento/src/main/java/test/PersonaPrueba.java
+++ Encapsulamiento/src/main/java/test/PersonaPrueba.java
@@ -3,11 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package test;
-
+import dominio.*;
 /**
  *
  * @author educu
  */
 public class PersonaPrueba {
-    
+    public static void main(String[] args) {
+        
+    }
 }
