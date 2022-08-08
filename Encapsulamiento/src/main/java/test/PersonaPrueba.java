e7ac86d0d8bc3841ddc35d2f7b793f7bfb5a413d
cumbinar <cumbiweb@gmail.com>
2022-08-07 21:17:29 -0500

    aplicate get and set in PersonaPrueba class

--- Encapsulamiento/src/main/java/test/PersonaPrueba.java
+++ Encapsulamiento/src/main/java/test/PersonaPrueba.java
@@ -3,13 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package test;
-import dominio.*;
+import dominio.Persona;
 /**
  *
  * @author educu
  */
 public class PersonaPrueba {
     public static void main(String[] args) {
+        Persona persona1 = new Persona("Cumbi", 5500.50, false);
+        System.out.println("El nombre de la persona 1 es => " + persona1.getNombre());
+        
+        persona1.setNombre("Cumbi Tara");
+        
+        System.out.println("El nombre modificado de persona 1 es => "+ persona1.getNombre());
         
     }
 }
