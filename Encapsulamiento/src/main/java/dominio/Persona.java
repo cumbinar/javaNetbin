d6155ce740bdaefb9ed6f84291dfd311f7ac1574
cumbinar <cumbiweb@gmail.com>
2022-08-07 20:52:31 -0500

    actualize Encapsulamiento/src/

---
+++ Encapsulamiento/src/main/java/dominio/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author educu
+ */
+public class Persona {
+    private String nombre;
+    private double sueldo;
+    private boolean eliminado;
+}
