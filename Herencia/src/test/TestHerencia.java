c8f602dea5f1b5ac4d684bd3ec4df269916f0fdd
cumbinar <cumbiweb@gmail.com>
2022-08-14 20:32:08 -0500

    create new Cliente object

--- Herencia/src/test/TestHerencia.java
+++ Herencia/src/test/TestHerencia.java
@@ -3,15 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package test;
+
 import domain.Empleado;
+import domain.Cliente;
+import java.util.Date;
 
 /**
  *
  * @author educu
  */
 public class TestHerencia {
+
     public static void main(String[] args) {
-       Empleado empleado1 = new Empleado("Cumbi", 131313.23);
-        System.out.println("empleado1"+empleado1);
+        Empleado empleado1 = new Empleado("Cumbi", 131313.23);
+        System.out.println("empleado1" + empleado1);
+
+        Cliente cliente1 = new Cliente(new Date(), true, "Guara Kaso", 'F', 28, "calle 13 20 30");
+        System.out.println("Datos del Cliente 1: => " + cliente1);
+
     }
 }
