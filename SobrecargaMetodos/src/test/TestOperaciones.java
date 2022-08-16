381e7e88eb5938621cac3f0c94c83d59db57937e
cumbinar <cumbiweb@gmail.com>
2022-08-16 16:11:58 -0500

    create Test class with invocar sumar methods

---
+++ SobrecargaMetodos/src/test/TestOperaciones.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import operaciones.Operaciones;
+
+/**
+ *
+ * @author educu
+ */
+public class TestOperaciones {
+    public static void main(String[] args) {
+        var resultado = Operaciones.sumar(5,3);
+        System.out.println("Resultado = "+ resultado);
+    }
+ 
+}
