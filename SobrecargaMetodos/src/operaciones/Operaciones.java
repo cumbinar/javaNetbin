636d8f485b4c806bdeef828543af7036d52c7e69
cumbinar <cumbiweb@gmail.com>
2022-08-16 16:06:05 -0500

    create sumar methods in Operaciones class

--- SobrecargaMetodos/src/operaciones/Operaciones.java
+++ SobrecargaMetodos/src/operaciones/Operaciones.java
@@ -9,5 +9,12 @@ package operaciones;
  * @author educu
  */
 public class Operaciones {
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
     
+    public static double sumar(double a, double b){
+    return a+b;
+    }
 }
