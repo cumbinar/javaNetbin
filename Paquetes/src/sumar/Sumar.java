39b1eb8fc9d4c49ec792873123d26512dceed12a
cumbinar <cumbiweb@gmail.com>
2022-08-17 16:41:48 -0500

    create Paquetes/src/sumar/

---
+++ Paquetes/src/sumar/Sumar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumar;
+
+/**
+ *
+ * @author educu
+ */
+public class Sumar {
+
+    public static float sumando(float a, float b) {
+        float suma = a + b;
+        return suma;
+    }
+
+}
