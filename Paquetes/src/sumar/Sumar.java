7eb0cae54d41bbdf4818dc66f59afb3896924f1e
cumbinar <cumbiweb@gmail.com>
2022-08-18 09:48:54 -0500

    add resta method

--- Paquetes/src/sumar/Sumar.java
+++ Paquetes/src/sumar/Sumar.java
@@ -12,9 +12,15 @@ public class Sumar {
 
     public static void sumando(float a, float b) {
         float suma = a + b;
+        float resta = a -b;
+        
+        
         System.out.println("la suma es "+ suma);
+        System.out.println("La resta es "+resta);
       
     }
+    
+    
  public static void imprimiendo(String s) {
         System.out.println(s);
     }
